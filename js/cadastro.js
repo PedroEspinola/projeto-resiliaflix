0cd51320b66a4afe9fb427933e9a3d6ed31d0316
Nelson <punkpunkada@gmail.com>
2021-09-13 23:11:09 -0300

    preparando JS - Requisição - Criação de Obj pessoa

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,15 +1,52 @@
+document.getElementById("btnPesquisar").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+document.getElementById("btnLimparEnd").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+document.getElementById("btnClean").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+document.getElementById("btnCadastrar").addEventListener("click", function(event){
+    event.preventDefault()
+});  
+  
 function pesquisaCEP(){
     
 }
 function limparCampos(){
-
+    $('#nome').val('');
+    $('#snome').val('');
+    $('#datanasc').val('');
+    $('#cel').val('');
+    $('#sex').val('');
+    $('#cpf').val('');
+    $('#email').val('');
+    $('#estciv').val('');
+    $('#cep').val('');
+    $('#rua').val('');
+    $('#num').val('');
+    $('#comp').val('');
+    $('#bairro').val('');
+    $('#city').val('');
+    $('#est').val('');
+   
 }
 function limparCEP(){
-
+    $('#cep').val('');
+    $('#rua').val('');
+    $('#num').val('');
+    $('#comp').val('');
+    $('#bairro').val('');
+    $('#city').val('');
+    $('#est').val('');
 }
 
 function cadastrar(){
-
+     
 }
 
 class Pessoa{
@@ -23,7 +60,7 @@ class Pessoa{
         this.sex = sex,
         this.estciv = estciv
     }
-    imprimirDados(){
+    imprimirDadosPessoais(){
         console.log( this.id,
             this.nome,
             this.snome,
@@ -35,3 +72,17 @@ class Pessoa{
     }
 
 }
+
+/*teste*/
+class Endereco extends Pessoa{
+    constructor(cep,rua,num,comp,bairro,city,est){
+        this.cep = cep,
+        this.rua = rua,
+        this.num = num,
+        this.comp = comp,
+        this.bairro = bairro,
+        this.city = city,
+        this.est = est
+    }
+}
+
